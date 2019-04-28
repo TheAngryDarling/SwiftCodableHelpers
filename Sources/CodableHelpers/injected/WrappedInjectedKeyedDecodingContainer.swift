76457fddc67cd2fec392639902e93da3ecda3fe2
Tyler Anger <tyler@tylers-macbook-pro.local>
2019-04-28 14:09:43 -0400

    Changed inheritance from KeyedDecodingContainerProtocol to WrappedKeyedDecodingContainer to support overriding codingPath.  Changed injectionKeys, injectionValues to one dictionary with basic struct that stores both.  Updated init's to support customCodingPath parameter and use of injections (instead of injectionKeys, injectionValues).  Updated all references from self.container... to use super... instead

--- Sources/CodableHelpers/injected/WrappedInjectedKeyedDecodingContainer.swift
+++ Sources/CodableHelpers/injected/WrappedInjectedKeyedDecodingContainer.swift
@@ -6,12 +6,12 @@
 //
 
 import Foundation
+import Nillable
 
 /*
  A wrapped Keyed Decoding Container that allows you to inject or replace key/value pair within the decoding process
  */
-public class WrappedInjectedKeyedDecodingContainer<K>: KeyedDecodingContainerProtocol where K: CodingKey {
-    public typealias Key = K
+public class WrappedInjectedKeyedDecodingContainer<Key>: WrappedKeyedDecodingContainer<Key> where Key: CodingKey {
     
     /*private struct HashedKey: Hashable {
         
@@ -36,145 +36,152 @@ public class WrappedInjectedKeyedDecodingContainer<K>: KeyedDecodingContainerPro
     }*/
     
     //private var injections: [HashedKey: Any] = [:]
-    private var injectionKeys: [Key] = []
-    private var injectionValues: [Any] = []
+    private var injections: [(key: Key, value: Any)] = []
+    //private var injectionKeys: [Key] = []
+    //private var injectionValues: [Any] = []
     
-    private var container: KeyedDecodingContainer<Key>
+    //private var container: KeyedDecodingContainer<Key>
     
     //public var codingPath: [CodingKey] { return self.container.codingPath }
-    public var codingPath: [CodingKey]
-    public var allKeys: [K] {
-        var rtn: [K] = self.container.allKeys
-        for k in self.injectionKeys {
-            if !rtn.contains(where: { $0.stringValue ==  k.stringValue &&  $0.intValue ==  k.intValue }) {
-                rtn.append(k)
+    //public var codingPath: [CodingKey]
+    public override var allKeys: [Key] {
+        var rtn: [Key] = super.allKeys
+        for i in self.injections {
+            if !rtn.contains(where: { $0.stringValue ==  i.key.stringValue &&  $0.intValue ==  i.key.intValue }) {
+                rtn.append(i.key)
             }
         }
         return rtn
     }
     
-    public init<Container>(_ container: Container, injections: [(key: Key, value: Any)]) where K == Container.Key, Container : KeyedDecodingContainerProtocol {
-        self.container = KeyedDecodingContainer<Key>(container)
-        self.codingPath = container.codingPath
-        for kv in injections {
-            self.injectionKeys.append(kv.key)
-            self.injectionValues.append(kv.value)
-        }
+    public init<Container>(_ container: Container,
+                           customCodingPath: [CodingKey]? = nil,
+                           injections: [(key: Key, value: Any)]) where Key == Container.Key, Container : KeyedDecodingContainerProtocol {
+        self.injections = injections
+        super.init(container, customCodingPath: customCodingPath)
     }
     
-    public convenience init<Container>(_ container: Container, injections: [String: Any]) where K == Container.Key, Container : KeyedDecodingContainerProtocol {
+    public convenience init<Container>(_ container: Container,
+                                       customCodingPath: [CodingKey]? = nil,
+                                       injections: [String: Any]) where Key == Container.Key, Container : KeyedDecodingContainerProtocol {
         var injs: [(key: Key, value: Any)] = []
         for (k,v) in injections {
             if let key = Key(stringValue: k) {
                 injs.append((key: key, value: v))
             }
         }
-        self.init(container, injections: injs)
+        self.init(container, customCodingPath: customCodingPath, injections: injs)
     }
     
-    public convenience init<Container>(_ container: Container, injection: (key: String, value: Any)) where K == Container.Key, Container : KeyedDecodingContainerProtocol {
-        self.init(container, injections: [injection.key: injection.value])
+    public convenience init<Container>(_ container: Container,
+                                       customCodingPath: [CodingKey]? = nil,
+                                       injection: (key: String, value: Any)) where Key == Container.Key, Container : KeyedDecodingContainerProtocol {
+        self.init(container, customCodingPath: customCodingPath, injections: [injection.key: injection.value])
     }
     
     private func injectionValue(forKey key: Key) -> Any? {
-        for (i,k) in self.injectionKeys.enumerated() {
-            if k.stringValue == key.stringValue && k.intValue == key.intValue {
-                return self.injectionValues[i]
+        for i in self.injections {
+            if i.key.stringValue == key.stringValue && i.key.intValue == key.intValue {
+                return i.value
             }
         }
         return nil
     }
     
     private func containsInjectionKey(_ key: Key) -> Bool {
-        for k in self.injectionKeys {
-            if k.stringValue == key.stringValue && k.intValue == key.intValue {
+        for i in self.injections {
+            if i.key.stringValue == key.stringValue && i.key.intValue == key.intValue {
                 return true
             }
         }
         return false
     }
     
-    public func contains(_ key: K) -> Bool {
+    public override func contains(_ key: Key) -> Bool {
         //return self.injections.keys.contains(HashedKey(key)) || self.container.contains(key)
-        return self.containsInjectionKey(key) || self.container.contains(key)
+        return self.containsInjectionKey(key) || super.contains(key)
     }
     
-    public func decodeNil(forKey key: K) throws -> Bool {
-        return try self.container.decodeNil(forKey: key)
+    public override func decodeNil(forKey key: Key) throws -> Bool {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? Nillable else { return try super.decodeNil(forKey: key) }
+        return v.isNil
     }
     
-    public func decode(_ type: Bool.Type, forKey key: K) throws -> Bool {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? Bool else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: Bool.Type, forKey key: Key) throws -> Bool {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? Bool else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: String.Type, forKey key: K) throws -> String {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? String else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: String.Type, forKey key: Key) throws -> String {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? String else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: Double.Type, forKey key: K) throws -> Double {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? Double else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: Double.Type, forKey key: Key) throws -> Double {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? Double else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: Float.Type, forKey key: K) throws -> Float {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? Float else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: Float.Type, forKey key: Key) throws -> Float {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? Float else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: Int.Type, forKey key: K) throws -> Int {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? Int else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: Int.Type, forKey key: Key) throws -> Int {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? Int else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: Int8.Type, forKey key: K) throws -> Int8 {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? Int8 else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: Int8.Type, forKey key: Key) throws -> Int8 {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? Int8 else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: Int16.Type, forKey key: K) throws -> Int16 {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? Int16 else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: Int16.Type, forKey key: Key) throws -> Int16 {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? Int16 else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: Int32.Type, forKey key: K) throws -> Int32 {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? Int32 else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: Int32.Type, forKey key: Key) throws -> Int32 {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? Int32 else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: Int64.Type, forKey key: K) throws -> Int64 {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? Int64 else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: Int64.Type, forKey key: Key) throws -> Int64 {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? Int64 else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: UInt.Type, forKey key: K) throws -> UInt {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? UInt else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: UInt.Type, forKey key: Key) throws -> UInt {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? UInt else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: UInt8.Type, forKey key: K) throws -> UInt8 {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? UInt8 else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: UInt8.Type, forKey key: Key) throws -> UInt8 {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? UInt8 else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: UInt16.Type, forKey key: K) throws -> UInt16 {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? UInt16 else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: UInt16.Type, forKey key: Key) throws -> UInt16 {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? UInt16 else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: UInt32.Type, forKey key: K) throws -> UInt32 {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? UInt32 else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: UInt32.Type, forKey key: Key) throws -> UInt32 {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? UInt32 else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode(_ type: UInt64.Type, forKey key: K) throws -> UInt64 {
-        guard let aV = self.injectionValue(forKey: key), let v = aV as? UInt64 else { return try self.container.decode(type, forKey: key) }
+    public override func decode(_ type: UInt64.Type, forKey key: Key) throws -> UInt64 {
+        guard let aV = self.injectionValue(forKey: key), let v = aV as? UInt64 else { return try super.decode(type, forKey: key) }
         return v
     }
     
-    public func decode<T>(_ type: T.Type, forKey key: K) throws -> T where T : Decodable {
-        guard let aV = self.injectionValue(forKey: key) else { return try self.container.decode(type, forKey: key) }
+    public override func decode<T>(_ type: T.Type, forKey key: Key) throws -> T where T : Decodable {
+        guard let aV = self.injectionValue(forKey: key) else {
+            //print("\(self.codingPath.appending(key).stringPath)")
+            return try super.decode(type, forKey: key)
+        }
         if let v = aV as? T { return v }
         else if let d = aV as? [String: Any] {
             let injector = InjectedKeyedDecodingContainer<CodableKey>(self.codingPath.appending(key), injections: d)
@@ -193,8 +200,9 @@ public class WrappedInjectedKeyedDecodingContainer<K>: KeyedDecodingContainerPro
         }
     }
     
-    public func nestedContainer<NestedKey>(keyedBy type: NestedKey.Type, forKey key: K) throws -> KeyedDecodingContainer<NestedKey> where NestedKey : CodingKey {
-        guard let aV = self.injectionValue(forKey: key) else { return try self.container.nestedContainer(keyedBy: type, forKey: key) }
+    public override func nestedContainer<NestedKey>(keyedBy type: NestedKey.Type,
+                                                    forKey key: Key) throws -> KeyedDecodingContainer<NestedKey> where NestedKey : CodingKey {
+        guard let aV = self.injectionValue(forKey: key) else { return try super.nestedContainer(keyedBy: type, forKey: key) }
         if let d = aV as? [String: Any] {
             let injector = InjectedKeyedDecodingContainer<CodableKey>(self.codingPath.appending(key), injections: d)
             let bridge = BridgedKeyedDecodingContainer<CodableKey, NestedKey>(injector)
@@ -204,20 +212,20 @@ public class WrappedInjectedKeyedDecodingContainer<K>: KeyedDecodingContainerPro
         }
     }
     
-    public func nestedUnkeyedContainer(forKey key: K) throws -> UnkeyedDecodingContainer {
-        guard let aV = self.injectionValue(forKey: key) else { return try self.container.nestedUnkeyedContainer(forKey: key) }
+    public override func nestedUnkeyedContainer(forKey key: Key) throws -> UnkeyedDecodingContainer {
+        guard let aV = self.injectionValue(forKey: key) else { return try super.nestedUnkeyedContainer(forKey: key) }
         var newPath: [CodingKey] = []
         newPath.append(contentsOf: self.codingPath)
         newPath.append(key)
         return InjectedUnkeyedDecodingContainer(newPath, object: aV)
     }
     
-    public func superDecoder() throws -> Decoder {
-        return try self.container.superDecoder()
+    public override func superDecoder() throws -> Decoder {
+        return try super.superDecoder()
     }
     
-    public func superDecoder(forKey key: K) throws -> Decoder {
-        return try self.container.superDecoder(forKey: key)
+    public override func superDecoder(forKey key: Key) throws -> Decoder {
+        return try super.superDecoder(forKey: key)
     }
     
     
