97c0495e363c82e71d16eb3205d1c14433d2cbe0
Tyler Anger <tyler@tylers-macbook-pro.local>
2019-04-28 14:17:34 -0400

    Updated codingPath to be changeable Added change to codingPath initializeContainer for KeyedEncodingContainer to append new key

--- Sources/CodableHelpers/delayed/DelayedEncodingContainer.swift
+++ Sources/CodableHelpers/delayed/DelayedEncodingContainer.swift
@@ -12,7 +12,7 @@ import Foundation
 */
 public class DelayedEncodingContainer {
 
-    public let codingPath: [CodingKey]
+    public private(set) var codingPath: [CodingKey]
     public private(set) var wasContainerSet: Bool = false
     
     public init(codingPath: [CodingKey]) {
@@ -26,5 +26,6 @@ public class DelayedEncodingContainer {
     public func initializeContainer<ParentKey>(fromParent parent: inout KeyedEncodingContainer<ParentKey>,
                                                forKey key: ParentKey) throws /* where ParentKey : CodingKey */ {
         self.wasContainerSet = true
+        self.codingPath.append(key)
     }
 }
