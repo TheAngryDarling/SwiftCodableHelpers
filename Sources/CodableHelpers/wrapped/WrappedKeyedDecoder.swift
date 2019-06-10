43b88d2fd15bcfdce9a63b1045e28a661adafac8
Tyler Anger <tyler@tylers-macbook-pro.local>
2019-06-10 02:23:27 -0400

    Update to proper Xcode comment blocks for Xcode Quick View Documentation Added WrappedPreKeyedDecoder to act similar to WrappedKeyedDecoder but accesses child elements of the real container with the preKey.

--- Sources/CodableHelpers/wrapped/WrappedKeyedDecoder.swift
+++ Sources/CodableHelpers/wrapped/WrappedKeyedDecoder.swift
@@ -7,15 +7,14 @@
 
 import Foundation
 
-/*
- A decoder that wrapps a keyed decoding container.
- Unsupported functions: unkeyedContainer, singleValueContainer
-*/
+/// A decoder that wraps a keyed decoding container.
+///
+/// Unsupported functions: unkeyedContainer, singleValueContainer
 public class WrappedKeyedDecoder<K>: Decoder where K: CodingKey {
     
     public var userInfo: [CodingUserInfoKey : Any] = [:]
     internal var container: KeyedDecodingContainer<K>
-   // public var codingPath: [CodingKey] { return self.container.codingPath }
+   
     public let codingPath: [CodingKey]
     public init(_ container: KeyedDecodingContainer<K>) {
         self.container = container
@@ -38,5 +37,41 @@ public class WrappedKeyedDecoder<K>: Decoder where K: CodingKey {
     
     public func singleValueContainer() throws -> SingleValueDecodingContainer {
         fatalError("Unsupported method")
+    }
+}
+
+/// A decoder that wraps a keyed decoding container.
+///
+/// Unsupported functions: unkeyedContainer, singleValueContainer
+public class WrappedPreKeyedDecoder<K>: Decoder where K: CodingKey {
+    public var userInfo: [CodingUserInfoKey : Any] = [:]
+    internal var container: KeyedDecodingContainer<K>
+    private let preKey: K
+    // public var codingPath: [CodingKey] { return self.container.codingPath }
+    public let codingPath: [CodingKey]
+    public init(_ container: KeyedDecodingContainer<K>, preKey: K) {
+        self.container = container
+        self.preKey = preKey
+        self.codingPath = container.codingPath
+    }
+    
+    public init<Container>(_ container: Container, preKey: K) where K == Container.Key, Container : KeyedDecodingContainerProtocol {
+        self.container = KeyedDecodingContainer<K>(container)
+        self.preKey = preKey
+        self.codingPath = container.codingPath
+    }
+    
+    public func container<Key>(keyedBy type: Key.Type) throws -> KeyedDecodingContainer<Key> where Key : CodingKey {
+        return try self.container.nestedContainer(keyedBy: type, forKey: self.preKey)
+    }
+    
+    public func unkeyedContainer() throws -> UnkeyedDecodingContainer {
+        return try self.container.nestedUnkeyedContainer(forKey: self.preKey)
+    }
+    
+    public func singleValueContainer() throws -> SingleValueDecodingContainer {
+        let uk = try self.container.nestedUnkeyedContainer(forKey: self.preKey)
+        if let r = uk as? SingleValueDecodingContainer { return r }
+        else { fatalError("Unsupported method") }
     }
 }
