bbf14842e84d8f2f4db604c5a97eb7996b23ac65
Tyler Anger <tyler@tylers-macbook-pro.local>
2019-06-10 02:32:20 -0400

    Update to proper Xcode comment blocks for Xcode Quick View Documentation Updated the Encoder/Decoder constructors to take an optional boxing/unboxing protocol to allow overriding of the default boxing/unboxing methods

--- Sources/CodableHelpers/coders/SimpleEncoder.swift
+++ Sources/CodableHelpers/coders/SimpleEncoder.swift
@@ -1,5 +1,5 @@
 //
-//  DictionaryEncoder.swift
+//  SimpleEncoder.swift
 //  CodableHelpers
 //
 //  Created by Tyler Anger on 2018-11-06.
@@ -7,12 +7,10 @@
 
 import Foundation
 
-/*
- A Simple encoder that converts objects into their base encoded data set eg, base types, arrays, dictionaries
-*/
+/// A Simple encoder that converts objects into their base encoded data set eg, base types, arrays, dictionaries
 public final class SimpleEncoder: BasicOpenEncoder<Any> {
     public init() {
-        super.init() { e, v in
+        super.init(boxing: nil) { e, v in
             return v
         }
     }
