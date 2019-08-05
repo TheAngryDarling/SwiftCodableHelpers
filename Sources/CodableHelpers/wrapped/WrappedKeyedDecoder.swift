ca892dfe7a27df68df03403866d3141152f415a2
Tyler Anger <tyler@tylers-macbook-pro.local>
2019-08-05 11:55:38 -0400

    Removed WrappedPreKeyedDecoder from file into separate file

--- Sources/CodableHelpers/wrapped/WrappedKeyedDecoder.swift
+++ Sources/CodableHelpers/wrapped/WrappedKeyedDecoder.swift
@@ -40,38 +40,4 @@ public class WrappedKeyedDecoder<K>: Decoder where K: CodingKey {
     }
 }
 
-/// A decoder that wraps a keyed decoding container.
-///
-/// Unsupported functions: unkeyedContainer, singleValueContainer
-public class WrappedPreKeyedDecoder<K>: Decoder where K: CodingKey {
-    public var userInfo: [CodingUserInfoKey : Any] = [:]
-    internal var container: KeyedDecodingContainer<K>
-    private let preKey: K
-    // public var codingPath: [CodingKey] { return self.container.codingPath }
-    public let codingPath: [CodingKey]
-    public init(_ container: KeyedDecodingContainer<K>, preKey: K) {
-        self.container = container
-        self.preKey = preKey
-        self.codingPath = container.codingPath
-    }
-    
-    public init<Container>(_ container: Container, preKey: K) where K == Container.Key, Container : KeyedDecodingContainerProtocol {
-        self.container = KeyedDecodingContainer<K>(container)
-        self.preKey = preKey
-        self.codingPath = container.codingPath
-    }
-    
-    public func container<Key>(keyedBy type: Key.Type) throws -> KeyedDecodingContainer<Key> where Key : CodingKey {
-        return try self.container.nestedContainer(keyedBy: type, forKey: self.preKey)
-    }
-    
-    public func unkeyedContainer() throws -> UnkeyedDecodingContainer {
-        return try self.container.nestedUnkeyedContainer(forKey: self.preKey)
-    }
-    
-    public func singleValueContainer() throws -> SingleValueDecodingContainer {
-        let uk = try self.container.nestedUnkeyedContainer(forKey: self.preKey)
-        if let r = uk as? SingleValueDecodingContainer { return r }
-        else { fatalError("Unsupported method") }
-    }
-}
+
